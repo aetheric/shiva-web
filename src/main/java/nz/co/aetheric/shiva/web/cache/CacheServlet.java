e75d5734af11e717aa3e705a3c532452711a0e92
Peter Cummuskey <peterc@aetheric.co.nz>
2014-01-28 12:14:18 +1300

    Set up the skeleton required for providing cache-manifest generation.

---
+++ src/main/java/nz/co/aetheric/shiva/web/cache/CacheServlet.java
@@ -0,0 +1,56 @@
+package nz.co.aetheric.shiva.web.cache;
+
+import net.jawr.web.context.JawrContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * TODO: What is the purpose of this class?
+ * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
+ */
+public class CacheServlet extends HttpServlet {
+	private static final Logger logger = LoggerFactory.getLogger(CacheServlet.class);
+
+	public static final String CACHE_MIME = "text/cache-manifest";
+	public static final String CACHE_HEAD = "CACHE MANIFEST";
+	public static final String CACHE_SEC_CACHE = "CACHE:";
+	public static final String CACHE_SEC_NET = "NETWORK:";
+	public static final String CACHE_SEC_FALL = "FALLBACK:";
+
+	/**
+	 * TODO
+	 * @param req The HTTP request.
+	 * @param resp The HTTP response.
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType(CACHE_MIME);
+
+		PrintWriter writer = resp.getWriter();
+
+		writer.println(CACHE_HEAD);
+		// TODO: Last modified.
+
+		writer.println(CACHE_SEC_CACHE);
+		// TODO: Add JAWR bundles, etc.
+
+		writer.println(CACHE_SEC_NET);
+		// TODO: API urls
+		writer.println("*");
+
+		writer.println(CACHE_SEC_FALL);
+		//TODO: Fallback urls.
+
+		writer.flush();
+	}
+}
---
+++ src/main/java/nz/co/aetheric/shiva/web/cache/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * TODO: What is the purpose of this class?
+ * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
+ */
+package nz.co.aetheric.shiva.web.cache;
